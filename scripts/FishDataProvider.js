2dbfc3de211d545ea3680265d5330e6d78b172d8
William Daniel Blackman <blackmanbilly@gmail.com>
2020-02-26 13:54:59 -0600

    created FishDataProvider.js and added fish list array

---
+++ scripts/FishDataProvider.js
@@ -0,0 +1,52 @@
+const fishCollection = [
+    {
+        name: "Bart",
+        species: "Blue Tang",
+        food: ["algae", "crustaceans"],
+        'harvest location': "Atlantic Ocean",
+        length: "4 feet"
+    },
+
+    {
+        name: "Trey",
+        species: "Chinese Goldfish",
+        food: "kibble",
+        'harvest location': "Yangtze River",
+        length: "2 feet"
+
+    },
+
+    {
+        name: "James",
+        species: "Great White Shark",
+        food: "People",
+        'harvest location': "Atlantic Ocean",
+        length: "15 feet"
+    },
+
+    {
+        name: "Alex",
+        species: "Gross-Brained Koi",
+        food: "PLankton",
+        'harvest location': "Bermuda",
+        length: "5 inches"
+    },
+
+    {
+        name: "Alex",
+        species: "Gross-Brained Koi",
+        food: "PLankton",
+        'harvest location': "Bermuda",
+        length: "5 inches"
+    },
+
+    {
+        name: "Margot",
+        species: "Purple Flounder",
+        food: "PLankton",
+        'harvest location': "Great Barrier Reed",
+        length: "4 inches"
+    },
+    
+
+]
