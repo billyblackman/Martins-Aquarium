e8cea8f515be34899a62fd98e818b28c9c7b9e8f
William Daniel Blackman <blackmanbilly@gmail.com>
2020-02-27 13:31:26 -0600

    created modular js to automate html fish creation

---
+++ scripts/FishList.js
@@ -0,0 +1,41 @@
+import { useFish } from "./FishDataProvider.js"
+import Fish from "./Fish.js"
+/**
+ *  FishList which renders individual fish objects as HTML
+ */
+
+
+
+const FishList = () => {
+
+    // Get a reference to the `<article class="content">` element
+    const contentElement = document.querySelector("body")
+    const fishes = useFish()
+
+// Generate all of the HTML for all of the fish
+let fishHTMLRepresentations = ""
+for (const fish of fishes) {
+
+    fishHTMLRepresentations += Fish(fish)
+
+
+    /*
+        Invoke the Fish component function
+        and pass the current fish object as an argument.
+        Each time, add the return value to the
+        fishHTMLRepresentations variable with `+=`
+    */
+}
+
+// Add a section, and all of the fish to the DOM
+contentElement.innerHTML += `
+    <section class="fishList">
+        ${fishHTMLRepresentations}
+    </section>
+`
+
+}
+
+export default FishList
+
+
--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,14 @@
 import initializeDetailButtonEvents from './dialogs.js'
 import { useFish } from './FishDataProvider.js'
+import Fish from './Fish.js'
+import FishList from './FishList.js'
 
-initializeDetailButtonEvents()
+// initializeDetailButtonEvents()
 
 const allTheFishObjects = useFish()
 
 for (const currentFishObject of allTheFishObjects) {
     console.log(currentFishObject)
 }
+
+FishList()
