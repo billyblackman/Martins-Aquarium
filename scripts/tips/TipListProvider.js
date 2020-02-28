eedcf9ff7003ff80cb25349b2d608a46f42a455a
William Daniel Blackman <blackmanbilly@gmail.com>
2020-02-27 21:15:12 -0600

    added tip list automation javascript

---
+++ scripts/tips/TipListProvider.js
@@ -0,0 +1,10 @@
+const tipCollection = [
+    "Make sure tank has water",
+    "Clean poop out regularly",
+    "Feed the fish daily"
+]
+
+
+export const tipCollectionCopy = () => {
+    return tipCollection.slice()
+}
