b6f6941d9286b879d1710ee58d51218d4e925c20
William Daniel Blackman <blackmanbilly@gmail.com>
2020-02-28 11:18:54 -0600

    added images to locations.html

--- scripts/locations/Location.js
+++ scripts/locations/Location.js
@@ -3,8 +3,33 @@
  */
 const locationFunction = (location) => {
     return `
-        <section class="location">${location}</section>
+        <div>
+            <img class="location__image" src="${location.image}" alt="">
+        </div>
+        <div class="location">
+        <h3>${location.name}</h3>
+        
+        </div>
     `
 }
 
 export default locationFunction
+
+// return `
+//         <div class="fish">
+//             <div>
+//                 <img class="fish__image" src="${fish.image}" alt="">
+//             </div>
+//             <div class="fish__name">${fish.name}</div>
+//             <button id="button--${fish.name}">Details</button>
+
+//             <dialog class="dialog--fish" id="details--${fish.name}">
+//                 <div>Species: ${fish.species}</div>
+//                 <div>Location: ${fish.location}</div>
+//                 <div>Length: ${fish.length}</div>
+//                 <div>Food: ${fish.food}</div>
+
+//                 <button class="button--close">Close Dialog</button>
+//             </dialog>
+//         </div>
+//     `
